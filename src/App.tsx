f8f88f5cafbe6cb094343b1ff703f1c52ef4225f
mjover <mjover@uncc.edu>
2023-07-19 17:22:23 -0400

    Adding base colors and type styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Heading one</h1>
+      <h2>Heading two</h2>
+      <h3>Heading three</h3>
+      <p>Paragraph paragraph paragraph, paragraph paragraph.</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#FFE833",
+      secondary: "#EFF3C2",
+      background: "#090D09",
+      body: "#F9FAF3",
+      subheading: "#BABAB5",
+      "spotify-green": "#1DB954",
+      "accent-1": "#FF9785",
+      "accent-2": "#33FF82",
+      "accent-3": "#A3AEFF",
+      "accent-4": "#33FFE8",
+      "accent-5": "#F28FFF",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
