6d4ab048fdaac958e8c6549813c6ca404569c659
mjover <mjover@uncc.edu>
2023-08-14 16:36:34 -0400

    Changing fluid spacing to static pixel values

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,16 +37,16 @@ module.exports = {
         "accent-5": "#F28FFF",
       },
       spacing: {
-        "3xs": "clamp(0.19rem, calc(0.08rem + 0.54vw), 0.50rem)", // max 8px
-        "2xs": "clamp(0.38rem, calc(0.16rem + 1.09vw), 1.00rem)", // max 16px
-        xs: "clamp(0.63rem, calc(0.32rem + 1.52vw), 1.50rem)", // max 24px
-        sm: "clamp(0.81rem, calc(0.40rem + 2.07vw), 2.00rem)", // max 32px
-        md: "clamp(1.00rem, calc(0.48rem + 2.61vw), 2.50rem)", // max 40px
-        lg: "clamp(1.19rem, calc(0.56rem + 3.15vw), 3.00rem)", // max 48px
-        xl: "clamp(1.38rem, calc(0.64rem + 3.70vw), 3.50rem)", // max 56px
-        "2xl": "clamp(1.63rem, calc(0.80rem + 4.13vw), 4.00rem)", // max 64px
-        "3xl": "clamp(2.00rem, calc(0.96rem + 5.22vw), 5.00rem)", // max 80px
-        "4xl": "clamp(3.19rem, calc(1.51rem + 8.37vw), 8.00rem)", // max 128px
+        "3xs": "8px",
+        "2xs": "16px",
+        xs: "24px",
+        sm: "32px",
+        md: "40px",
+        lg: "48px",
+        xl: "56px",
+        "2xl": "64px",
+        "3xl": "80px",
+        "4xl": "128px",
       },
       fontSize: {
         xs: "clamp(0.75rem, 0.23vw + 0.69rem, 0.88rem)", // Pretitle
