ab9c52004d87015745134d5351263b80e03a1f03
mianjoto <mianjoto@gmail.com>
2023-08-24 12:26:26 -0400

    Increasing max-width of utopia container for xl screens

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -11,7 +11,7 @@ export function Hero({}) {
     <>
       <Section
         level="main"
-        className="h-screen w-full items-end overflow-hidden px-32 pb-64 md:grid-rows-none lg:px-64 lg:py-80"
+        className="h-screen w-full auto-rows-min content-center  items-end overflow-hidden px-32 pb-80 md:grid-rows-none lg:px-0 lg:pb-80 lg:pl-100"
       >
         {/* Content */}
         <div className="col-span-full row-start-2 lg:col-span-6 lg:row-start-1">
@@ -26,12 +26,12 @@ export function Hero({}) {
           </Link>
         </div>
         {/* Disc */}
-        <div className="col-span-full row-span-full row-start-1 flex flex-col items-end lg:col-span-6 lg:block">
+        <div className="col-span-full row-span-full row-start-1 flex flex-col items-end lg:col-span-6 lg:block lg:h-3/4">
           {/* -md:right-1/5 absolute -bottom-1/4 -right-[60%] col-span-full md:absolute md:col-span-6 md:h-full md:overflow-hidden */}
           <Disc
             ringColor="primary"
             spin="clockwise"
-            className="relative -right-[12.5%] h-auto w-full max-w-md overflow-x-hidden md:max-w-2xl lg:static lg:bottom-0 lg:right-0 lg:max-w-3xl xl:min-w-[110%] xl:max-w-6xl"
+            className="relative -right-[12.5%] h-auto w-full max-w-md sm:w-[clamp(31.25rem,_-81.25rem_+_150vw,_50rem)] md:max-w-2xl lg:max-w-3xl xl:bottom-[18vh] xl:right-[5%] xl:max-w-6xl"
           />
         </div>
       </Section>
