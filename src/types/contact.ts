49d3da581d32ff4b6288861dca188e1a96faecc7
mianjoto <mianjoto@gmail.com>
2023-08-30 16:57:34 -0400

    Adding ContactData object for personal information

---
+++ src/types/contact.ts
@@ -0,0 +1,9 @@
+export const ContactData = {
+  name: process.env.DEV_NAME,
+  username: "mianjoto",
+  links: {
+    linkedIn: "https://linkedin.com/in/mianjoto",
+    github: "https://github.com/mianjoto",
+    portfolio: "https://mianjoto.github.io/",
+  },
+};
