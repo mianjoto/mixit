06f413586810c30124dca38e20a1aeb7325c0ce2
mianjoto <mianjoto@gmail.com>
2023-09-23 12:20:44 -0400

    Add JWT refresh token rotation callback

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,38 @@
 import { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "./spotify";
+import spotifyApi, { LOGIN_URL } from "./spotify";
+import { JWT } from "next-auth/jwt";
+
+interface Token extends JWT {
+  username: string;
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpires: number;
+}
+
+async function refreshAccessToken(token: Token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("Spotify refresh token after being refreshed=", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const authConfig: NextAuthOptions = {
   providers: [
@@ -11,4 +43,28 @@ export const authConfig: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: (account?.expires_at as number) * 1000,
+        } as Token;
+      }
+
+      // Return previous token if access token is valid
+      if (Date.now() < (token as Token).accessTokenExpires) {
+        console.log("Existing Spotify API access token is valid");
+        return token as Token;
+      }
+
+      // Access token is expired, refresh
+      console.log("Spotify API access token has expired, refreshing...");
+      return await refreshAccessToken(token as Token);
+    },
+  },
 };
