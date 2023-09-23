3a5d57ab58eed365ea593222b0f265da17f3c6ef
mianjoto <mianjoto@gmail.com>
2023-09-23 13:55:13 -0400

    Add NextAuth sessionprovider

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,23 @@
+import { Session } from "next-auth";
+
 import { DashboardRootLayout } from "@/components/dashboard";
 import DashboardNavbarContent from "@/components/dashboard-navbar";
+import { NextAuthSessionProvider } from "@/providers/next-auth-session-provider";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: Session;
+}) => {
   return (
-    <div className="flex flex-col lg:flex-row">
-      <DashboardNavbarContent />
-      <DashboardRootLayout>{children}</DashboardRootLayout>
-    </div>
+    <NextAuthSessionProvider session={session}>
+      <div className="flex flex-col lg:flex-row">
+        <DashboardNavbarContent />
+        <DashboardRootLayout>{children}</DashboardRootLayout>
+      </div>
+    </NextAuthSessionProvider>
   );
 };
 
