12aaeb53fb35d45d0b6c68eeb3da429837a232a2
mjover <mjover@uncc.edu>
2023-10-06 12:33:56 -0400

    Change Mixit logo to navigate to dashboard in /dashboard

--- src/components/ui/mixit-home-logo.tsx
+++ src/components/ui/mixit-home-logo.tsx
@@ -3,16 +3,21 @@ import { MixitLogo } from "@/assets/mixit";
 import Link from "next/link";
 import { cn } from "../../../lib/utils";
 
-interface MixitIconProps extends React.SVGProps<SVGSVGElement> {}
+interface MixitIconProps extends React.SVGProps<SVGSVGElement> {
+  linkToDashboard?: boolean;
+}
 
 const MixitHomeLogo: React.FC<MixitIconProps> = ({
+  linkToDashboard = false,
   width,
   height,
   className,
   ...props
 }) => {
+  const link = linkToDashboard ? "/dashboard" : "/";
+
   return (
-    <Link href="/" aria-label="Mixit" className="w-fit">
+    <Link href={link} aria-label="Mixit" className="w-fit">
       <MixitLogo
         {...props}
         width={width ? width : "70px"}
