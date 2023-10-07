110d44e940b3ddbd18511bc7902b8a614d999d92
mjover <mjover@uncc.edu>
2023-10-07 16:30:53 -0400

    Add RadixUI separators between app form buttons

--- src/app/(dashboard)/shuffler/page.tsx
+++ src/app/(dashboard)/shuffler/page.tsx
@@ -4,7 +4,6 @@ import { Apps } from "@/types/apps";
 import { AppFormCard } from "@/components/app-form-card";
 import { DashboardHeading } from "../../../components/dashboard";
 import { DashboardShelf } from "@/components/dashboard-shelf";
-import Separator from "@/components/ui/separator";
 
 export default function Shuffler() {
   const app = Apps.Shuffler;
@@ -13,7 +12,7 @@ export default function Shuffler() {
       <AppDashboardHeading app={app} />
       <section className="flex flex-col gap-16">
         <DashboardHeading text={"What would you like to shuffle?"} />
-        <DashboardShelf desktopBehavior={"shelf"}>
+        <DashboardShelf desktopBehavior={"shelf"} withSeparators>
           <AppFormCard.LikedSongs app={app} />
           <AppFormCard.Playlists app={app} />
         </DashboardShelf>
--- src/components/dashboard-shelf.tsx
+++ src/components/dashboard-shelf.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { cva, VariantProps } from "class-variance-authority";
-import { HTMLAttributes } from "react";
+import React, { HTMLAttributes } from "react";
 import { cn } from "../../lib/utils";
 import { DashboardCard } from "./dashboard-card";
+import * as Separator from "@radix-ui/react-separator";
 
 const dashboardShelfVariants = cva("", {
   variants: {
@@ -25,7 +28,9 @@ const dashboardShelfVariants = cva("", {
 
 export interface DashboardShelfProps
   extends HTMLAttributes<HTMLElement>,
-    VariantProps<typeof dashboardShelfVariants> {}
+    VariantProps<typeof dashboardShelfVariants> {
+  withSeparators?: boolean;
+}
 
 const generateLoadingCards = (numberOfCards: number = 3) => {
   const cards = [];
@@ -40,8 +45,21 @@ const LOADING_CARDS = generateLoadingCards();
 export const DashboardShelf = ({
   desktopBehavior,
   mobileBehavior,
+  withSeparators = false,
   children,
 }: DashboardShelfProps) => {
+  let content;
+
+  if (children === undefined) {
+    content = LOADING_CARDS;
+  }
+
+  if (withSeparators && children !== undefined) {
+    content = separateContent(children);
+  } else {
+    content = children;
+  }
+
   return (
     <section
       className={cn(
@@ -51,7 +69,32 @@ export const DashboardShelf = ({
         })
       )}
     >
-      {children || LOADING_CARDS}
+      {content}
     </section>
   );
 };
+
+function separateContent(content: React.ReactNode) {
+  const contentArray = React.Children.toArray(content);
+  if (contentArray.length === 1) {
+    return contentArray;
+  }
+
+  return contentArray.map((element, index) => {
+    const isLastElement = index === contentArray.length - 1;
+
+    if (isLastElement) {
+      return element;
+    }
+
+    return (
+      <>
+        {element}
+        <Separator.Root
+          orientation="vertical"
+          className="my-40 w-[3px] bg-[#D9D9D9]/20"
+        />
+      </>
+    );
+  });
+}
