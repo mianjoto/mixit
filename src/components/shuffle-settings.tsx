39c30ca01319b8008ad548036e6f35f32d056f24
mianjoto <mianjoto@gmail.com>
2023-10-16 17:48:56 -0400

    Add key prop to mapped options

--- src/components/shuffle-settings.tsx
+++ src/components/shuffle-settings.tsx
@@ -58,6 +58,7 @@ const renderOptions = (app: Apps) => {
       <ShuffleSettingOption
         description={option.description}
         id={option.id}
+        key={option.id}
         checkedBgColorVariant={selectedAppColorVariant}
       />
     );
