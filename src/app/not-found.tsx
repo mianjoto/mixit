7cd86b7f9bc880d858b1d742da20d1f0a94a8aa3
mianjoto <mianjoto@gmail.com>
2023-10-25 23:45:41 -0400

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/footer";
+import { LandingNavbar } from "@/components/landing-navbar";
+import NotFoundSection from "@/components/not-found-section";
+
+export default function NotFound() {
+  return (
+    <>
+      <LandingNavbar />
+      <NotFoundSection />
+      <Footer />
+    </>
+  );
+}
