78e8d0774edb71ba2f06de87d065538c5c06690b
mianjoto <mianjoto@gmail.com>
2023-11-07 13:09:40 -0500

    Extend modal customization

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,12 +2,15 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { Cross1Icon } from "@radix-ui/react-icons";
 import { Heading } from "./ui/heading";
 import { HeadingLevels } from "@/types/text";
+import { cn } from "../../lib/utils";
 
 interface ModalProps {
   isOpen: boolean;
   onChange: (open: boolean) => void;
   title: string;
   description: string;
+  titleClassName?: string;
+  descriptionClassName?: string;
   children: React.ReactNode;
 }
 
@@ -16,6 +19,8 @@ const Modal: React.FC<ModalProps> = ({
   onChange,
   title,
   description,
+  titleClassName,
+  descriptionClassName,
   children,
 }) => {
   return (
@@ -23,12 +28,25 @@ const Modal: React.FC<ModalProps> = ({
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm" />
         <Dialog.Content className="fixed left-[50%] top-[50%] z-[100] h-full w-full translate-x-[-50%] translate-y-[-50%] overflow-y-auto rounded-md border border-neutral-700 bg-tertiary p-24 drop-shadow-md focus:outline-none md:h-auto md:max-h-[85vh] md:w-[90vw] md:max-w-[700px] md:px-24 md:py-40">
-          <Dialog.Title className="mb-4 transform-gpu text-center text-lg font-bold text-body md:transform-cpu">
+          <Dialog.Title
+            className={cn(
+              "mb-4 transform-gpu text-center text-lg font-bold text-body md:transform-cpu",
+              titleClassName
+            )}
+          >
             {title}
           </Dialog.Title>
-          <Dialog.Description className="mb-5 transform-gpu text-center text-sm leading-normal text-gray md:transform-cpu">
-            {description}
-          </Dialog.Description>
+          {description !== "" ? (
+            <Dialog.Description
+              className={cn(
+                "mb-5 transform-gpu text-center text-sm leading-normal text-gray md:transform-cpu",
+                descriptionClassName
+              )}
+            >
+              {description}
+            </Dialog.Description>
+          ) : null}
+
           <div className="flex transform-gpu flex-col items-center md:transform-cpu">
             {children}
           </div>
