9c8173772ba11a0e8fa1f13b7d279b3b69fed9ff
Miguel Jover <78712025+mianjoto@users.noreply.github.com>
2025-01-08 18:18:43 -0500

    Update Jest config and add simple test to shuffler

--- src/api/tests/shuffler.test.ts
+++ src/api/tests/shuffler.test.ts
@@ -1,3 +1,8 @@
-test("1 + 1 = 2", () => {
-  expect(1 + 1).toBe(2);
+import { shuffle } from "../lib/shuffler";
+
+describe("shuffle function shuffles items as expected", () => {
+    test("empty array returns empty array", () => {
+        const emptyArray = [];
+        expect(shuffle(emptyArray)).toBe(emptyArray);
+    });
 });
