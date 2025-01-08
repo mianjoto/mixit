53cf7c5fa183bee76d055267c3096762f346e267
Miguel Jover <78712025+mianjoto@users.noreply.github.com>
2025-01-08 18:34:24 -0500

    Add tests to assert shuffle function behavior

--- src/api/tests/shuffler.test.ts
+++ src/api/tests/shuffler.test.ts
@@ -2,7 +2,19 @@ import { shuffle } from "../lib/shuffler";
 
 describe("shuffle function shuffles items as expected", () => {
     test("empty array returns empty array", () => {
-        const emptyArray = [];
-        expect(shuffle(emptyArray)).toBe(emptyArray);
+        const emptyArray: Array<number> = [];
+        expect(shuffle(emptyArray)).toMatchObject(emptyArray);
+    });
+    test("items are shuffled in-place (modifying the initial array)", () => {
+        const initialArray = [1, 2, 3, 4, 5];
+        const shuffledArray = shuffle(initialArray);
+        expect(shuffledArray).toMatchObject(initialArray);
+    });
+    test("can shuffle array with mixed objects", () => {
+        const initialArray = ["one", 2, { three: 3 }, true];
+        const initialArrayShallowCopy = Array.from(initialArray);
+        expect(shuffle(initialArray)).not.toMatchObject(
+            initialArrayShallowCopy
+        );
     });
 });
