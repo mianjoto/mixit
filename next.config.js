f5b71ced9638e702f8cae38ac1530581f42fbb34
Miguel Jover <78712025+mianjoto@users.noreply.github.com>
2025-01-31 15:30:26 -0500

    Update Next, TS, types, project deps

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const config = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-    });
-    return config;
-  },
-  experimental: {
-    serverActions: true,
-  },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+        });
+        return config;
+    },
 };
 
 export default config;
